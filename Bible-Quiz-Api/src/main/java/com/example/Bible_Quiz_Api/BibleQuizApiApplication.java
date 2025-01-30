6a928f90e3320e4359b8e1f61132ebac9e1d0c6e
osmane <osmane.neto@hotmail.com>
2025-01-29 22:42:57 -0300

    .

---
+++ Bible-Quiz-Api/src/main/java/com/example/Bible_Quiz_Api/BibleQuizApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bible_Quiz_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleQuizApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleQuizApiApplication.class, args);
+	}
+
+}
---
+++ Bible-Quiz-Api/src/test/java/com/example/Bible_Quiz_Api/BibleQuizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bible_Quiz_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleQuizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
